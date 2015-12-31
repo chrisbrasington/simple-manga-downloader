c57cc52f2bda4407f3d45b871c0643aaf2121dd1
Christopher Brasington <chrisbrasington@gmail.com>
2015-12-31 21:37:54 +0000

    google search

--- program.rb
+++ program.rb
@@ -1,8 +1,32 @@
+require 'rubygems'
 require 'mechanize'
 
-test_url = "http://kissmanga.com/Manga/Seikai-no-Monshou-YONEMURA-Kouichirou"
+a = Mechanize.new { |agent|
+    agent.user_agent_alias = 'Linux Firefox'
+}
 
-mechanize = Mechanize.new
-page = mechanize.get(test_url)
+a.get('http://google.com/') do |page|
+    search_result = page.form_with(:name => 'f') do |search|
+        search.q = 'kissmanga Seikai no Monshou'
+end.submit
 
-puts page.title
+links = Array.new
+
+search_result.links.each do |link|
+    unless link.href.nil? 
+        if link.href.include? 'http://kissmanga' #and link.href.include? 'kissmanga'
+            puts link.text
+            puts link.href
+            puts '-----------'
+            links.push(link)
+        end
+    end
+end
+  
+url = links.first.href
+  
+#chapter = a.get(url)
+#puts chapter.title
+  
+  
+end
